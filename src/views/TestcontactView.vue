9ecd50751472b3ead23bdc4cc3482d51b89359d8
MarcosYH <marcosdeimos18@gmail.com>
2023-07-19 16:21:12 +0200

    Add feature/contact page

--- src/views/TestcontactView.vue
+++ src/views/TestcontactView.vue
@@ -1,15 +1,20 @@
 <template>
-  <div>
+    <div class="contact">
+      <h1> Contact</h1>
     
-  </div>
-</template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      display: flex;
+      align-items: center;
+    }
+  }
+  .contact{
+    position: relative;
+    top: 8rem;
+  }
+  </style>
+  
